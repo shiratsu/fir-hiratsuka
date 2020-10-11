69974cfd706851c9d32edb9db0c09dfb33d11780
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2020-10-11 22:45:18 +0900

    firebaseのサウンド用のPG

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const indexRouter = require('./routes/index');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.static(path.join(__dirname, 'routes')));
+app.use('/', indexRouter);
+
+exports.app = functions.https.onRequest(app);
